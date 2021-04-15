4dc5b0d07878e8adf698b8878662474d32214cda
Cat Weird <52168620+catware1@users.noreply.github.com>
2021-04-15 05:44:33 +0300

    Create choose.py

---
+++ choose.py
@@ -0,0 +1,14 @@
+# CatABMS-type executable
+author = "aGrIk"
+mode = "start"
+deps = 'None'
+identificator = 'choose'
+command_ru = 'выбери'
+description = 'Выбирает один элемент из нескольких перечисленных через "или" элементов'
+
+arr = parameter.split(" или ")
+
+choosers = "Кот выбрал,Древние оракулы выбрали,Сова выбрала, Разрабы котопая выбрали,Питон выбрал,Я выбрал,Рандом выбрал".split(",")
+
+message(randd.choice(choosers) + " " + randd.choice(arr), reply=True)
+del choosers
